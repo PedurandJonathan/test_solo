775bf95af496cf7a02b29fdd05c2cb4460a0dd8e
PedurandJonathan <74648766+PedurandJonathan@users.noreply.github.com>
2021-03-20 13:42:39 +0100

    test

--- test_todos.py
+++ test_todos.py
@@ -1,10 +1,15 @@
-from todos import TaskManager, Action
+from todos import  TaskManager, Action, Action2
 
 
 def test_can_create_an_action():
-    action = Action("add", "first task")
+    action = Action("add", "first task", 1)
     assert action.name == "add"
     assert action.description == "first task"
+    assert action.numero == 1
+
+def test_can_delete_an_action():
+    action = Action2()
+
 
 
 def test_can_create_a_task_manager():
@@ -20,16 +25,19 @@ def test_task_manager_starts_with_no_tasks():
 def test_parse_add():
     command = "+ first task"
     task_manager = TaskManager()
+    numero = 1
 
-    action = task_manager.parse(command)
+    action = task_manager.parseadd(command, numero)
 
     assert action.name == "add"
     assert action.description == "first task"
-    def test_parse_add():
-        command = "+ first task"
+    assert action.numero == 1
+
+def test_parse_del():
+    command = "- first task"
+    numero = 1
     task_manager = TaskManager()
 
-    action = task_manager.parse(command)
-
-    assert action.name == "add"
-    assert action.description == "first task"
+    action = task_manager.parsedel(command,numero)
+    assert action.name == "del"
+    assert action.numero == 1
--- todos.py
+++ todos.py
@@ -2,10 +2,26 @@ class TaskManager:
     def __init__(self):
         self.tasks = []
 
-    def parse(self, command):
+    def parseadd(self, command):
         return Action("add", command[2:])
 
+    def parsedel(self, numero):
+        return Action2("del", numero)
+
+
+
 class Action:
-    def __init__(self, name, description):
+    def __init__(self, name, description, numero):
         self.name = name
         self.description = description
+        self.numero = numero
+        
+class Action2:
+    def __init__(self):
+        pass
+
+
+#    def __del__(self):
+#        print('Destructor called') 
+#objet1 = Action2()
+#del objet1
